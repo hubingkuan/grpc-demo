8fe7168f390bd2c8bd0f857f8298a0f2896d0891
hubingkuan <1656956568@qq.com>
2023-06-29 19:31:31 +0800

    proto校验 示例

--- demo-2/proto/gen_go.sh
+++ demo-2/proto/gen_go.sh
@@ -2,15 +2,11 @@
 
 source ./proto_dir.cfg
 
-
-
+# 生成待校验的proto文件以及html接口文档
 for ((i = 0; i < ${#all_proto[*]}; i++)); do
-  protoFile=${all_proto[$i]}
-  protoName=$(basename "$protoFile" .proto)
-  protoc  --doc_out=./docs --doc_opt=html,$protoName.html --go_out=plugins=grpc,module=grpc-demo/demo-2/proto:./   $protoFile
-  echo "protoc --go_out=plugins=grpc:." $protoFile
+  protoPath=${all_proto[$i]}
+  protoName=$(basename "$protoPath" .proto)
+  protoc  --doc_out=./docs --doc_opt=html,$protoName.html --validate_out="lang=go,paths=source_relative:./" --go_out=plugins=grpc,module=grpc-demo/demo-2/proto:./   $protoPath
+  echo "protoc --go_out=plugins=grpc:." $protoPath
 done
 echo "proto file generate success..."
-
-
-#  --validate_out="lang=go:./gen"
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module grpc-demo
 go 1.20
 
 require (
+	github.com/envoyproxy/protoc-gen-validate v0.10.0
 	google.golang.org/grpc v1.55.0
 	google.golang.org/protobuf v1.30.0
 )
